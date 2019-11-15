04ab7944bad172344304b1f6691a06b89e1df4a8
Mohamed Bakli <mohamed_bakli@aun.edu.eg>
2019-11-15 17:51:22 +0100

    Add accessor functions

--- MobilityDB/TimeTypes/period.py
+++ MobilityDB/TimeTypes/period.py
@@ -3,18 +3,24 @@ from bdateutil.parser import parse
 
 
 class PERIOD:
-    __slots__ = ['lower', 'upper', 'lower_inc', 'upper_inc']
+    __slots__ = ['lowerT', 'upperT', 'lowerT_inc', 'upperT_inc']
 
     # Missing arguments (lower_inc, upper_inc)
     def __init__(self, lower=None, upper=None):
         if isinstance(lower, str) and isinstance(upper, str):
-            self.lower = parse(lower)
-            self.upper = parse(upper)
+            self.lowerT = parse(lower)
+            self.upperT = parse(upper)
         elif isinstance(lower, datetime.datetime) and isinstance(upper, datetime.datetime):
-            self.lower = lower
-            self.upper = upper
+            self.lowerT = lower
+            self.upperT = upper
         else:
             raise Exception("ERROR:  Could not parse period value")
 
+    def lower(self):
+        return self.lowerT
+
+    def upper(self):
+        return self.upperT
+
     def __str__(self):
-        return self.__class__.__name__+'({}, {})'.format(self.lower, self.upper)
+        return self.__class__.__name__+'({}, {})'.format(self.lowerT, self.upperT)
--- README.md
+++ README.md
@@ -27,16 +27,14 @@ Usage
     
     --Result is an object of TGEOMPOINTSEQ type:
     --TGEOMPOINT(SEQUENCE)'[POINT(1.0 2.0)@2019-09-08 00:00:00+02:00, POINT(0.0 2.0)@2019-09-09 00:00:00+02:00, POINT(1.0 1.0)@2019-09-10 00:00:00+02:00]'
+   
+   1.  Functions and Operators for Time Types and Range Types
     
-    --Accessor functions
-    getValue(ttype)
-    getValues(ttype)
-    getTimestamp(ttype)
-    timespan(ttype)
-    startInstant(ttype)
-    endInstant(ttype)
-    startValue(ttype)
-    endValue(ttype)
-    instantsN(ttype, n)
-    getType(ttype)
+    1.1 Constructor Functions
+        period(timestamptz, timestamptz, boolean = true, boolean = false):  period
+        timestampset(timestamptz[]}):  timestampset
+        
+    1.3  Accessor Functions
+        lower(period):  timestamptz
+        upper(period):  timestamptz
     
