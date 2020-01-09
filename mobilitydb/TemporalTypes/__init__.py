40fce1cb7e4d912d2350577c4adf635993e53039
Esteban Zimanyi <wit@cs.ulb.ac.be>
2020-01-09 10:02:32 +0100

    Apply Python naming conventions

---
+++ mobilitydb/TemporalTypes/__init__.py
@@ -0,0 +1,8 @@
+from .temporal import Temporal
+from .temporalinst import TemporalInst
+from .temporalinstants import TemporalInstants
+from .temporali import TemporalI
+from .temporalseq import TemporalSeq
+from .temporals import TemporalS
+
+__all__ = ['Temporal', 'TemporalInst', 'TemporalInstants', 'TemporalI', 'TemporalSeq', 'TemporalS']
